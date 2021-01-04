78ceceb2e79568f7bdf33ceedbe8604be1565def
abe menashy <abestbet1@gmail.com>
2021-01-03 17:28:12 -0800

    js classes

--- js/main.js
+++ js/main.js
@@ -36,52 +36,12 @@ class CountdownTracker {
       this.card.classList.add('flip');
     }
   }
-
-  // update(value)
 }
-
-// function CountdownTracker(label, value) {
-//   //create an individual card composed of 4 sections
-//   let card = document.createElement('div');
-//   card.className = 'flip-clock__piece';
-//   card.innerHTML = `<div class="flip-clock__card card"><div class="card__top"></div><div class="card__bottom"></div><div class="card__back"><div class="card__bottom"></div></div></div>
-//     <span class="flip-clock__slot">${label}</span>`;
-
-//   this.card = card;
-
-//   //access each of the 4 card components
-//   const cardTop = card.querySelector('.card__top');
-//   const cardBottom = card.querySelector('.card__bottom');
-//   const cardBack = card.querySelector('.card__back');
-//   const cardBackBottom = card.querySelector('.card__back .card__bottom');
-
-//   this.update = (val) => {
-//     val = ('0' + val).slice(-2);
-//     if (val !== this.currentValue) {
-//       if (this.currentValue >= 0) {
-//         //when card is flipping the bottom value of the card, and
-//         // the back of the top portion of the card will have the same value
-//         cardBack.setAttribute('data-value', this.currentValue);
-//         cardBottom.setAttribute('data-value', this.currentValue);
-//       }
-//       this.currentValue = val;
-//       cardTop.innerText = this.currentValue;
-//       cardBackBottom.setAttribute('data-value', this.currentValue);
-
-//       this.card.classList.remove('flip');
-//       // TO-DO what is void doing?
-//       void this.card.offsetWidth;
-//       this.card.classList.add('flip');
-//     }
-//   };
-
-//   this.update(value);
-// }
 
 // Calculation adapted from https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
 
 function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
+  const t = Date.parse(endtime) - Date.parse(new Date());
   return {
     Total: t,
     Days: Math.floor(t / (1000 * 60 * 60 * 24)),
@@ -93,7 +53,7 @@ function getTimeRemaining(endtime) {
 
 //returns default 12 hour timer
 function getTime() {
-  var t = new Date();
+  const t = new Date();
   return {
     Total: t,
     Hours: t.getHours() % 12,
@@ -142,7 +102,7 @@ class Clock {
       this.card.appendChild(trackers[key].card);
     }
 
-    var i = 0;
+    let i = 0;
     const updateClock = () => {
       timeinterval = requestAnimationFrame(updateClock);
 
@@ -153,7 +113,7 @@ class Clock {
       }
 
       // if timer has ended
-      var t = this.updateFn(this.countdown);
+      const t = this.updateFn(this.countdown);
 
       if (t.Total < 0) {
         cancelAnimationFrame(timeinterval);
@@ -174,75 +134,8 @@ class Clock {
   }
 }
 
-// function Clock(countdown, callback) {
-//   // if countdown parameter passed in create new date
-//   countdown = countdown ? new Date(Date.parse(countdown)) : false;
-
-//   // if no callback function passed in, assign value empty function
-//   callback = callback || function () {};
-
-//   // if countdown value exists return object with time values
-//   // else return object with time values for 12 hours
-//   const updateFn = countdown ? getTimeRemaining : getTime;
-
-//   //create container for cards
-//   this.card = document.createElement('div');
-//   this.card.className = 'flip-clock';
-
-//   let trackers = {};
-//   let t = updateFn(countdown);
-//   let key;
-//   let timeinterval;
-
-//   //t should have values of Total, Days, Hours, Minutes, Seconds
-//   for (key in t) {
-//     if (key === 'Total') {
-//       // if 'Total' skip
-//       continue;
-//     }
-
-//     //assign each key in the object returned from updateFn to
-//     // the trackers object
-
-//     //pass in the key and the value
-//     trackers[key] = new CountdownTracker(key, t[key]);
-
-//     //append the card propery from CountdownTracker to the cardContainer
-//     console.log(this.card);
-//     this.card.appendChild(trackers[key].card);
-//   }
-
-//   var i = 0;
-//   function updateClock() {
-//     timeinterval = requestAnimationFrame(updateClock);
-
-//     // throttle so it's not constantly updating the time.
-//     // only return value from modulo operation of 1 will be true
-//     if (i++ % 10) {
-//       return;
-//     }
-
-//     // if timer has ended
-//     var t = updateFn(countdown);
-//     if (t.Total < 0) {
-//       cancelAnimationFrame(timeinterval);
-//       for (key in trackers) {
-//         trackers[key].update(0);
-//       }
-//       callback();
-//       return;
-//     }
-//     //else if there is still time remaining update value
-//     for (key in trackers) {
-//       trackers[key].update(t[key]);
-//     }
-//   }
-
-//   setTimeout(updateClock, 500);
-// }
-
-var deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
-var c = new Clock(deadline, function () {
+const deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
+const c = new Clock(deadline, function () {
   alert('countdown complete');
 });
 c.start();
